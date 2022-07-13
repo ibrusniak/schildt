37e3d76a85d1ed05263c3afd29cb8fd639bfadc0
ibrusniak <ivan.brusniak@gmail.com>
2022-07-13 08:01:09 +0300

    Examine `java.util.List`. Create 'testing framework'

---
+++ 19-COLLECTIONS/2/ListDemo1.java
@@ -0,0 +1,16 @@
+
+import static utils.MyUtills.*;
+
+import javax.swing.border.EmptyBorder;
+
+public class ListDemo1 {
+
+    public static void main(String[] args) {
+
+        
+    }
+
+}
+
+
+
---
+++ 19-COLLECTIONS/2/utils/MyUtills.java
@@ -0,0 +1,19 @@
+
+package utils;
+
+public final class MyUtills {
+
+    public static void emptyline() {
+        System.out.println();
+    }
+
+    public static <E> void print(E message) {
+        System.out.print(message);
+    }
+
+    public static <E> void println(E message) {
+        System.out.println(message);
+    }
+}
+
+
