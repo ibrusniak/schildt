fed26607803f29679d59c4975d3106b3f405795c
ibrusniak <ivan.brusniak@gmail.com>
2022-08-06 09:35:13 +0300

    Add service functions No file to print and

---
+++ 19-COLLECTIONS/3/utils/Printers.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class Printers {
+    
+    public static <T> void print(T obj) {
+        System.out.print(obj);
+    }
+    
+    public static <T> void println(T obj) {
+        System.out.println(obj);
+    }
+}
