1a8f5aa13c495ee120e8fea0e143d3ef7b859485
ibrusniak <ivan.brusniak@gmail.com>
2022-08-28 08:40:31 +0300

    Examine `java.util.Collections` algorithms

---
+++ 19-COLLECTIONS/14/CollectionsDemo.java
@@ -0,0 +1,15 @@
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import static java.util.Collections.*;
+
+public class CollectionsDemo {
+
+    public static void main(String[] args) {
+        
+        
+    }
+}
+
