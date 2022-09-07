9294e8a1556cfb8caf4a7c468cf9b92790553d60
Ivan Brusniak <ivan.brusniak@gmail.com>
2022-09-07 19:28:34 +0300

    Examine java.lang.Void

---
+++ Ch18/7/VoidDemo.java
@@ -0,0 +1,10 @@
+public class VoidDemo {
+    
+    public static void main(String[] args) {
+        
+        System.out.println(Void.class.getName());   // java.lang.Void
+        System.out.println(Void.TYPE);              // void
+        System.out.println(Void.TYPE.getName());    // void
+    }
+}
+
