8c1a0d168e2c4139b256e193939a7e0b8d4a7abe
Ivan Brusniak <ivan.brusniak@gmail.com>
2022-09-08 16:05:52 +0300

    Use Character.toString(codePoint) to see emoji

---
+++ Ch18/7/UnicodeCharacter.java
@@ -0,0 +1,29 @@
+import javax.swing.plaf.synth.SynthButtonUI;
+
+public class UnicodeCharacter {
+    private static final String USAGE = """
+
+            UnicodeCharacter - prints unicode character info by its code.
+            Usage: java UnicodeCharacter code,
+            where:
+                code -  hexadecimal code like 0x1F615 or 0X1F615
+                        or simple decimal code 128533
+            
+            """;
+    private static final String WR = Character.toString(0x1F615)
+        + "\nSomething went wrong!\n";
+    
+    public static void main(String[] args) {
+        try {
+            int codePoint = Integer.decode(args[0]);
+            String symbol = Character.toString(codePoint);
+            System.out.printf("hex: %1$h dec: %1$d name: \'%2$s\' sym: \'",
+                codePoint, Character.getName(codePoint));
+            System.out.println(symbol + "\'");
+        } catch (Exception e) {
+            System.out.print(WR + USAGE);
+            e.printStackTrace();
+        }
+    }    
+}
+
