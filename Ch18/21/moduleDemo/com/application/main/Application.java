4dfc64280868d89d6421973d6bb5ae2f1147677e
Ivan Brusniak <ivan.brusniak@gmail.com>
2022-09-20 09:35:57 +0300

    Work with java modules

---
+++ Ch18/21/moduleDemo/com/application/main/Application.java
@@ -0,0 +1,11 @@
+
+package com.application.main;
+
+public class Application {
+
+    public static void main(String[] args) {
+        
+        System.out.println("com.application.main.Application.main()");
+    }
+}
+
