9c0c3f0189418658cc8ca05bfe520f228d08d6fd
Ivan Brusniak <ivan.brusniak@gmail.com>
2022-09-20 10:17:17 +0300

    Work with java modules

---
+++ Ch18/22/module1/mainpackage/App.java
@@ -0,0 +1,17 @@
+
+package mainpackage;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        System.out.println("debug #0");
+        ArrayList<String> list = new ArrayList<>();
+        list.add("debug #1");
+        JOptionPane.showMessageDialog(null, "debug #3");
+    }
+}
+
---
+++ Ch18/22/module1/module-info.java
@@ -0,0 +1,4 @@
+
+module module1 {
+    requires java.desktop;
+}
